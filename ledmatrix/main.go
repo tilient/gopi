39191d830729aa6d9f9c25b1b13322bb4f5d2420
wiffel <wiffel@tilient.org>
2017-03-24 23:11:52 +0000

    adapted ledmatrix

--- ledmatrix/main.go
+++ ledmatrix/main.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"bitbucket.org/gmcbay/i2c"
+	"bitbucket.org/gmcbay/i2c/HT16K33"
 )
 
 func main() {
@@ -16,15 +17,28 @@ func main() {
 	bp.WriteByte(0x70, 0x81, 0x00)
 	bp.WriteByte(0x70, 0xe0, 0x00)
 
-	for t := 0; t < 100; t++ {
+	devices, err := HT16K33.ParseDevices("0x70:1")
+	if err != nil {
+		log.Panicf("failed to parse devices: %v\n", err)
+	}
+	HT16K33.ScrollMessage("wiffel", devices, 255)
+
+	for t := 0; t < 10; t++ {
 		for ix := 0; ix < 16; ix += 2 {
 			bp.WriteByte(0x70, byte(ix), 0xcc)
 		}
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(80 * time.Millisecond)
 		for ix := 0; ix < 16; ix += 2 {
 			bp.WriteByte(0x70, byte(ix), 0xff)
 		}
-		time.Sleep(200 * time.Millisecond)
+		time.Sleep(80 * time.Millisecond)
 	}
+	for t := 0; t < 10; t++ {
+		bp.WriteByte(0x70, 0xe0, 0x00)
+		time.Sleep(80 * time.Millisecond)
+		bp.WriteByte(0x70, 0xef, 0x00)
+		time.Sleep(80 * time.Millisecond)
+	}
+	bp.WriteByte(0x70, 0xe0, 0x00)
 
 }
