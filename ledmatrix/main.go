299274f8624db8b254bb3ccb2dfca6510b61f4d4
wiffel <wiffel@tilient.org>
2017-04-17 18:14:58 +0200

    ledmatrix changes

--- ledmatrix/main.go
+++ ledmatrix/main.go
@@ -5,7 +5,7 @@ import (
 	"time"
 
 	"bitbucket.org/gmcbay/i2c"
-	"bitbucket.org/gmcbay/i2c/HT16K33"
+	//"bitbucket.org/gmcbay/i2c/HT16K33"
 )
 
 func main() {
@@ -17,28 +17,39 @@ func main() {
 	bp.WriteByte(0x70, 0x81, 0x00)
 	bp.WriteByte(0x70, 0xe0, 0x00)
 
-	devices, err := HT16K33.ParseDevices("0x70:1")
-	if err != nil {
-		log.Panicf("failed to parse devices: %v\n", err)
-	}
-	HT16K33.ScrollMessage("wiffel", devices, 255)
-
-	for t := 0; t < 10; t++ {
-		for ix := 0; ix < 16; ix += 2 {
-			bp.WriteByte(0x70, byte(ix), 0xcc)
-		}
-		time.Sleep(80 * time.Millisecond)
-		for ix := 0; ix < 16; ix += 2 {
-			bp.WriteByte(0x70, byte(ix), 0xff)
-		}
-		time.Sleep(80 * time.Millisecond)
-	}
-	for t := 0; t < 10; t++ {
-		bp.WriteByte(0x70, 0xe0, 0x00)
-		time.Sleep(80 * time.Millisecond)
+	//devices, err := HT16K33.ParseDevices("0x71:1,0x70:1")
+	//if err != nil {
+	//		log.Panicf("failed to parse devices: %v\n", err)
+	//}
+	for {
 		bp.WriteByte(0x70, 0xef, 0x00)
-		time.Sleep(80 * time.Millisecond)
+		bp.WriteByte(0x71, 0xef, 0x00)
+
+		//HT16K33.ScrollMessage("wiffel & linda",
+		//  devices, 200)
+
+		for t := 0; t < 10; t++ {
+			for ix := 0; ix < 16; ix += 2 {
+				bp.WriteByte(0x70, byte(ix), 0xcc)
+				bp.WriteByte(0x71, byte(ix), 0xcc)
+			}
+			time.Sleep(80 * time.Millisecond)
+			for ix := 0; ix < 16; ix += 2 {
+				bp.WriteByte(0x70, byte(ix), 0xff)
+				bp.WriteByte(0x71, byte(ix), 0xff)
+			}
+			time.Sleep(80 * time.Millisecond)
+		}
+		for t := 0; t < 10; t++ {
+			bp.WriteByte(0x70, 0xe0, 0x00)
+			bp.WriteByte(0x71, 0xe0, 0x00)
+			time.Sleep(80 * time.Millisecond)
+			bp.WriteByte(0x70, 0xef, 0x00)
+			bp.WriteByte(0x71, 0xef, 0x00)
+			time.Sleep(80 * time.Millisecond)
+		}
+		bp.WriteByte(0x70, 0xe0, 0x00)
+		bp.WriteByte(0x71, 0xe0, 0x00)
 	}
-	bp.WriteByte(0x70, 0xe0, 0x00)
 
 }
