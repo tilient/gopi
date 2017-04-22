6ebfbad8123544ee1700c3d46b3f0fb7d5c9d716
wiffel <wiffel@tilient.org>
2017-04-22 11:39:11 +0200

    small refactorings

--- ledclock/main.go
+++ ledclock/main.go
@@ -29,132 +29,124 @@ func main() {
 // ===========================================================
 
 type PixelMatrix struct {
-	spi               *spi.Device
-	nrOfMatrices      int
-	nrOfRowsPerMatrix int
-	buffer            []byte
-	font              [][]byte
+	bus           *spi.Device
+	nrOfMatrices  int
+	rowsPerMatrix int
+	buffer        []byte
+	font          [][]byte
 }
 
 func newPixelMatrix(devstr string, brightness byte,
-	nrOfMatrices, nrOfRowsPerMatrix int,
+	nrOfMatrices, rowsPerMatrix int,
 	font [][]byte) *PixelMatrix {
-	spi, err := spi.Open(
+	bus, err := spi.Open(
 		&spi.Devfs{devstr, spi.Mode0, 4000000})
 	if err != nil {
 		log.Fatal(err)
 	}
-	buffer := make([]byte, nrOfMatrices*nrOfRowsPerMatrix)
-	this := &PixelMatrix{
-		spi:               spi,
-		nrOfMatrices:      nrOfMatrices,
-		nrOfRowsPerMatrix: nrOfRowsPerMatrix,
-		buffer:            buffer,
-		font:              font,
+	buffer := make([]byte, nrOfMatrices*rowsPerMatrix)
+	pm := &PixelMatrix{
+		bus:           bus,
+		nrOfMatrices:  nrOfMatrices,
+		rowsPerMatrix: rowsPerMatrix,
+		buffer:        buffer,
+		font:          font,
 	}
-	this.sendCmd(MAX7219_REG_SCANLIMIT, 7)
-	this.sendCmd(MAX7219_REG_DECODEMODE, 0)
-	this.sendCmd(MAX7219_REG_DISPLAYTEST, 0)
-	this.sendCmd(MAX7219_REG_SHUTDOWN, 1)
-	this.sendCmd(MAX7219_REG_INTENSITY, brightness)
-	this.clear()
-	this.flush()
-	return this
+	pm.sendCmd(max7219_SCAN_LIMIT, 7)
+	pm.sendCmd(max7219_DECODE_MODE, 0)
+	pm.sendCmd(max7219_DISPLAY_TEST, 0)
+	pm.sendCmd(max7219_SHUTDOWN, 1)
+	pm.sendCmd(max7219_INTENSITY, brightness)
+	pm.clear()
+	pm.flush()
+	return pm
 }
 
-func (this *PixelMatrix) Close() {
-	this.spi.Close()
+func (pm *PixelMatrix) Close() {
+	pm.bus.Close()
 }
 
 // ===========================================================
 
-func (this *PixelMatrix) flush() {
-	buf := make([]byte, 2*this.nrOfMatrices)
-	for line := 0; line < this.nrOfRowsPerMatrix; line++ {
-		for matrix := 0; matrix < this.nrOfMatrices; matrix++ {
-			buf[matrix*2] = byte(MAX7219_REG_DIGIT0 + line)
+func (pm *PixelMatrix) flush() {
+	buf := make([]byte, 2*pm.nrOfMatrices)
+	for line := 0; line < pm.rowsPerMatrix; line++ {
+		for matrix := 0; matrix < pm.nrOfMatrices; matrix++ {
+			buf[matrix*2] = byte(max7219_DIGIT0 + line)
 			buf[matrix*2+1] =
-				this.buffer[matrix*this.nrOfRowsPerMatrix+line]
+				pm.buffer[matrix*pm.rowsPerMatrix+line]
 		}
-		this.spi.Tx(buf[:], nil)
+		pm.bus.Tx(buf[:], nil)
 	}
 }
 
 // ===========================================================
 
 const (
-	MAX7219_REG_NOOP   byte = 0
-	MAX7219_REG_DIGIT0      = iota
-	MAX7219_REG_DIGIT1
-	MAX7219_REG_DIGIT2
-	MAX7219_REG_DIGIT3
-	MAX7219_REG_DIGIT4
-	MAX7219_REG_DIGIT5
-	MAX7219_REG_DIGIT6
-	MAX7219_REG_DIGIT7
-	MAX7219_REG_DECODEMODE
-	MAX7219_REG_INTENSITY
-	MAX7219_REG_SCANLIMIT
-	MAX7219_REG_SHUTDOWN
-	MAX7219_REG_DISPLAYTEST = 0x0F
+	max7219_DIGIT0       = 1
+	max7219_DECODE_MODE  = 9
+	max7219_INTENSITY    = 10
+	max7219_SCAN_LIMIT   = 11
+	max7219_SHUTDOWN     = 12
+	max7219_DISPLAY_TEST = 0x0F
 )
 
-func (this *PixelMatrix) sendCmd(register, value byte) {
+func (pm *PixelMatrix) sendCmd(register, value byte) {
 	data := []byte{register, value}
-	for matrix := 0; matrix < this.nrOfMatrices; matrix++ {
-		this.spi.Tx(data, nil)
+	for matrix := 0; matrix < pm.nrOfMatrices; matrix++ {
+		pm.bus.Tx(data, nil)
 	}
 }
 
 // ===========================================================
 
-func (this *PixelMatrix) clear() {
-	for ix := range this.buffer {
-		this.buffer[ix] = 0
+func (pm *PixelMatrix) clear() {
+	for ix := range pm.buffer {
+		pm.buffer[ix] = 0
 	}
 }
 
-func (this *PixelMatrix) setPixel(x, y int) {
+func (pm *PixelMatrix) setPixel(x, y int) {
 	if x < 0 {
 		return
 	}
 	if y < 0 {
 		return
 	}
-	if y >= this.nrOfRowsPerMatrix {
+	if y >= pm.rowsPerMatrix {
 		return
 	}
-	if x >= (8 * this.nrOfMatrices) {
+	if x >= (8 * pm.nrOfMatrices) {
 		return
 	}
-	line := this.nrOfRowsPerMatrix - 1 - y
+	line := pm.rowsPerMatrix - 1 - y
 	matrix := x / 8
 	bit := (byte)(1 << (uint)(7-(x%8)))
-	this.buffer[matrix*this.nrOfRowsPerMatrix+line] |= bit
+	pm.buffer[matrix*pm.rowsPerMatrix+line] |= bit
 }
 
 // ===========================================================
 
-func (this *PixelMatrix) plotString(str string, xPos int) int {
+func (pm *PixelMatrix) plotString(str string, xPos int) int {
 	x := xPos
 	for _, ch := range str {
-		x = this.plotChar((byte)(ch), x)
+		x = pm.plotChar((byte)(ch), x)
 		x += 1
 	}
 	return x
 }
 
-func (this *PixelMatrix) plotChar(ch byte, xPos int) int {
+func (pm *PixelMatrix) plotChar(ch byte, xPos int) int {
 	x := xPos
-	bitlines := this.font[ch-(byte)(' ')]
-	for _, bitline := range bitlines {
-		bitpos := (byte)(128)
+	bitLines := pm.font[ch-(byte)(' ')]
+	for _, bitLine := range bitLines {
+		bitPos := (byte)(128)
 		for y := 0; y < 8; y++ {
-			bit := bitline & bitpos
+			bit := bitLine & bitPos
 			if bit > 0 {
-				this.setPixel(x, y)
+				pm.setPixel(x, y)
 			}
-			bitpos >>= 1
+			bitPos >>= 1
 		}
 		x += 1
 	}
