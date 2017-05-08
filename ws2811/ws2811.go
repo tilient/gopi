2f3441fca727331161a2218b52a23826294c2ab8
wiffel <wiffel@tilient.net>
2017-05-09 00:00:57 +0200

    ws2811 adapted

--- ws2811/ws2811.go
+++ ws2811/ws2811.go
@@ -33,8 +33,8 @@ See https://github.com/jgarff/rpi_ws281x for instructions
 package ws2811
 
 /*
-#cgo CFLAGS: -std=c99
-#cgo LDFLAGS: -lws2811
+#cgo CFLAGS: -std=c99 -static
+#cgo LDFLAGS: -lws2811 -static
 #include "ws2811.go.h"
 */
 import "C"
